da8c1d791903e0836f95a65902d8e984d2681daf
Ahmed Allawi <ahmed.allawi92@gmail.com>
2023-08-15 12:52:44 -0700

    edit docs

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,11 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 -
- * return always success.
+ * main - Prints alphabet 10 times.
+ *
+ * Return: Always 0.
  */
+
 void print_alphabet_x10(void)
 {
 	int count = 0;
